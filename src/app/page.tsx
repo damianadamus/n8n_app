c8a74d62153bec87999bf0a74fba56770ef53c0b
Damian Adamus <adamus.damian@yahoo.com>
2025-11-30 19:49:51 +0100

    Testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
+import {cn} from "@/lib/utils";
+import {allowedDisplayValues} from "next/dist/compiled/@next/font/dist/constants";
+
 const Page = () => {
     return (
-        <div className="text-red-500 font-extrabold">Hello there!</div>
+        <>
+        <div className={cn("text-red-500 font-extrabold")}>Hello there!</div>
+        <div>Another div</div>
+        </>
     )
 }
 export default Page
