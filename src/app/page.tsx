25143bfa57e47fcb973ef6ec3e82c47194fcb00c
Damian Adamus <adamus.damian@yahoo.com>
2025-12-05 23:08:38 +0100

    Testing client component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+"use client"
+
 import { Button } from "@/components/ui/button";
 import {allowedDisplayValues} from "next/dist/compiled/@next/font/dist/constants";
+import {useEffect} from "react";
 
 const Page = () => {
+    useEffect(() => {}, []);
     return (
         <div className="min-h-screen min-w-screen flex items-center justify-center">
             <Button>
