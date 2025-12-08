dab1ae7f18bf033f08c7198e5aff6cd726d7b8fa
Damian Adamus <adamus.damian@yahoo.com>
2025-12-08 21:55:02 +0100

    Instaling tRPC

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,13 @@
-"use client"
 
-import { Button } from "@/components/ui/button";
-import {allowedDisplayValues} from "next/dist/compiled/@next/font/dist/constants";
-import {useEffect} from "react";
 
-const Page = () => {
-    useEffect(() => {}, []);
+import prisma from "@/lib/db";
+
+const Page= async () => {
+    const users = await prisma.user.findMany();
+
     return (
         <div className="min-h-screen min-w-screen flex items-center justify-center">
-            <Button>
-                Click me
-            </Button>
-            <Button>
-                Delete
-            </Button>
+            {JSON.stringify(users)}
         </div>
     )
 }
