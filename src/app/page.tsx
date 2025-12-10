f968278192bad0f1cd3c57452cb2d686737f9c94
Damian Adamus <adamus.damian@yahoo.com>
2025-12-10 21:06:01 +0100

    Implementing hydration using tRPC

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,27 @@
-import { caller } from '@/trpc/server';
+//import { caller } from '@/trpc/server';
+import { getQueryClient, trpc } from "@/trpc/server";
+//"use client"
+//import { useTRPC } from "@/trpc/client";
+//import { useQuery } from "@tanstack/react-query";
+import { Client } from "./client";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
 
 const Page= async () => {
-    const users = await caller.getUsers();
+    //const users = await caller.getUsers();
+    //const trpc = useTRPC();
+    //const { data: users } = useQuery(trpc.getUsers.queryOptions());
+    const queryClient = getQueryClient();
+
+    void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
 
     return (
         <div className="min-h-screen min-w-screen flex items-center justify-center">
-            {JSON.stringify(users)}
+            <HydrationBoundary state={dehydrate(queryClient)}>
+                <Suspense fallback={<p>Loading...</p>}>
+                    <Client />
+                </Suspense>
+            </HydrationBoundary>
         </div>
     )
 }
